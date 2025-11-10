9cb59c5965e270bd51f8dc974b22b7d5b73088e4
Matias Rinne <147260381+Niria@users.noreply.github.com>
2025-11-10 11:04:22 +0200

    Refactored the run loop and win state calculation
    Also added methods to calculate the value of rows containing the latest move, but they are not used for anything yet

--- src/game_board.py
+++ src/game_board.py
@@ -3,20 +3,38 @@ from enum import IntEnum
 
 class Markers(IntEnum):
     EMPTY = 0
-    HUMAN = 1
+    PLAYER = 1
     AI = 2
 
 
 class GameBoard:
     SYMBOLS = {
         Markers.EMPTY: ".",
-        Markers.HUMAN: "X",
+        Markers.PLAYER: "X",
         Markers.AI: "O"
+    }
+
+    WIN_VALUE = 100
+
+    VALUES = {
+        "11111": 100,
+        "011110": 80,
+        "011112": 40,
+        "211110": 40,
+        "10111": 40,
+        "11011": 40,
+        "11101": 40,
+        "01110": 25,
+        "01011": 25,
+        "01101": 25,
+        "01010": 10,
+        "01100": 10,
+        "00110": 10
     }
 
     def __init__(self, size):
         self.size = size
-        self.board = [[0 for _ in range(size)] for _ in range(size)]
+        self.board = [[Markers.EMPTY for _ in range(size)] for _ in range(size)]
         self.move_history = []
 
     def __str__(self):
@@ -38,39 +56,61 @@ class GameBoard:
     def undo_move(self):
         if self.move_history:
             col, row = self.move_history.pop()
-            self.board[row][col] = 0
+            self.board[row][col] = Markers.EMPTY
 
     def win_state(self):
-        move_col, move_row = self.move_history[-1]
-        marker = self.board[move_row][move_col]
+        rows = self.get_rows_containing_move(*self.move_history[-1])
 
-        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
-
-        for dx, dy in directions:
-            marker_count = 1
-
-            for i in range(1, 5):
-                col = move_col + i*dx
-                row = move_row + i*dy
-                if not self.valid_coordinate(col, row) or self.board[row][col] != marker:
-                    break
-                marker_count += 1
-
-            for i in range(1, 5):
-                col = move_col - i*dx
-                row = move_row - i*dy
-                if not self.valid_coordinate(col, row) or self.board[row][col] != marker:
-                    break
-                marker_count += 1
-            if marker_count >= 5:
+        for row in rows:
+            if abs(self.get_row_value(row)) == self.WIN_VALUE:
                 return True
         return False
+
+    def get_rows_containing_move(self, col, row):
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+        rows = []
+        move_col, move_row = col, row
+
+        for dx, dy in directions:
+            current_row = ""
+            for i in range(-4, 5):
+                col = move_col + i*dx
+                row = move_row + i*dy
+                if not self.valid_coordinate(col, row):
+                    continue
+                current_row += str(self.board[row][col])
+            rows.append(current_row)
+        return rows
 
     def get_children(self):
         pass
 
-    def value(self):
-        pass
+    def get_move_value(self, col, row, marker):
+        rows = self.get_rows_containing_move(col, row)
+        current_value = sum(self.get_row_value(row) for row in rows)
+
+        self.board[row][col] = marker
+        new_value = sum(self.get_row_value(row) for row in rows)
+        self.board[row][col] = Markers.EMPTY
+
+        return new_value - current_value
+
+    def get_row_value(self, row):
+        flip = {"0": "0", "1": "2", "2": "1"}
+        human_value = 0
+        ai_value = 0
+        flipped_row = "".join([flip[c] for c in row])
+
+        for pattern, value in self.VALUES.items():
+            if pattern in row:
+                human_value = -value
+                break
+            if pattern in flipped_row:
+                ai_value = value
+                break
+
+        return ai_value - human_value
+
 
     def valid_coordinate(self, col, row):
         if col < 0 or col >= self.size or row < 0 or row >= self.size:
--- src/gomoku.py
+++ src/gomoku.py
@@ -5,29 +5,40 @@ import random
 class Gomoku:
     def __init__(self, size=20):
         self.gameboard = GameBoard(size)
+        self.players_turn = True
+        self.current_value = 0
         self.valid_moves = []
 
     def run(self):
         while True:
-            user_input = input("X Y: ")
-            if user_input == "":
-                break
-            col, row = [int(n) for n in user_input.split(" ")]
-            valid_move = self.gameboard.move(col, row, Markers.HUMAN)
-            if not valid_move:
-                print(f"Invalid input, X and Y must be between 0 and {self.gameboard.size - 1}")
-                continue
-            self.__update_valid_moves(col, row)
+            if self.players_turn:
+                user_input = input("X Y: ")
+                if user_input == "":
+                    break
+
+                col, row = [int(n) for n in user_input.split(" ")]
+                valid_move = self.__player_move(col, row)
+
+                if not valid_move:
+                    print(f"Invalid input, X and Y must be between 0 and {self.gameboard.size - 1}")
+                    continue
+
+            else:
+                col, row = random.choice(self.valid_moves)
+                self.__ai_move(col, row)
+
             if self.gameboard.win_state():
-                print("Player won!")
+                if self.players_turn:
+                    print("Player won!")
+                else:
+                    print("AI won!")
                 print(self.gameboard)
                 break
-            else:
-                # Currently the AI just randomly picks a space
-                ai_x, ai_y = random.choice(self.valid_moves)
-                self.gameboard.move(ai_x, ai_y, Markers.AI)
-                print(f"Player placed X to ({col}, {row}), AI placed O to ({ai_x}, {ai_y})")
-            print(self.gameboard)
+
+            self.__update_valid_moves(col, row)
+            if not self.players_turn:
+                print(self.gameboard)
+            self.players_turn = not self.players_turn
 
     def __update_valid_moves(self, col, row):
         if (col, row) in self.valid_moves:
@@ -40,9 +51,10 @@ class Gomoku:
                 if self.gameboard.valid_coordinate(x, y) and self.gameboard.empty_space(x, y):
                     self.valid_moves.append((x, y))
 
+    def __player_move(self, col, row):
+        return self.gameboard.move(col, row, Markers.PLAYER)
 
-
-
-
+    def __ai_move(self, col, row):
+        return self.gameboard.move(col, row, Markers.AI)
 
 
